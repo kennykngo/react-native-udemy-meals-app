8449ba700e766be9707e689c4e9f3a5d843835bb
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-03-09 22:20:56 -0800

    added the navigationOptions to the CategoriesScreen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Platform,
+} from 'react-native';
+import Colors from '../constants/Color';
 
 import { CATEGORIES } from '../data/dummy-data';
 
-const renderGridItem = (itemData) => {
-  // itemData (part of renderItem)
-  // .item (one of the few properties of itemData object)
-  // .title (shape of object created from category.js)
+const CategoriesScreen = ({ navigation }) => {
+  const renderGridItem = (itemData) => {
+    // itemData (part of renderItem)
+    // .item (one of the few properties of itemData object)
+    // .title (shape of object created from category.js)
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          navigation.navigate('CategoryMeals');
+        }}
+      >
+        <View>
+          <Text> {itemData.item.title} </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View>
-      <Text> {itemData.item.title} </Text>
-    </View>
+    <FlatList
+      numColumns={2}
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderGridItem}
+    />
   );
 };
 
-const CategoriesScreen = ({ navigation }) => {
-  return (
-    <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem} />
-  );
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
@@ -25,6 +54,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
 
