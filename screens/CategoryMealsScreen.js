d2c422e1f6ec3a89f9bfc5a9373c088f15d5e73e
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-03-10 18:27:53 -0800

    passed mealId and captured that asa a variable to be used on MealDetailScreen.js

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,28 +1,46 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: 'MealDetail',
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+
   const categoryId = props.navigation.getParam('categoryId');
   const selectedCategory = CATEGORIES.find((cat) => cat.id === categoryId);
 
+  // will search through each categoryIds[] and check if the indexOf (catId) exists
+  // RETURNS a new array with the filtered elements
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(categoryId) >= 0
+  );
+
   return (
     <View style={styles.screen}>
-      <Text>CategoryMeals Screen!</Text>
-      {/* <Text>{categoryId}</Text> */}
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title='Go to Details'
-        onPress={() => {
-          props.navigation.navigate('MealDetail');
-        }}
-      />
-      <Button
-        title='Go Back'
-        onPress={() => {
-          props.navigation.goBack();
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   );
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+import { MEALS } from '../data/dummy-data';
+
 const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam('mealId');
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  console.log(mealId, selectedMeal);
+
   return (
     <View style={styles.screen}>
-      <Text>MealDetail Screen!</Text>
+      {/* <Text>{selectedMeal.title}</Text> */}
       <Button
         title='Go back to Categories'
         onPress={() => {
@@ -13,6 +20,17 @@ const MealDetailScreen = (props) => {
       />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('MealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  console.log(selectedMeal);
+
+  // return {
+  //   // headerTitle: selectedMeal.title,
+  // };
 };
 
 const styles = StyleSheet.create({
