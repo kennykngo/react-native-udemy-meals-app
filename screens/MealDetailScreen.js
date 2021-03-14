a2968ea87b38294cdf9bb21d7038c9908585fac4
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-03-13 19:01:35 -0800

    created ListItem components to contain the list items in MealDetails

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import DefaultText from '../components/DefaultText';
 import HeaderButton from '../components/HeaderButton';
 import { MEALS } from '../data/dummy-data';
 
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText> {props.children}</DefaultText>
+    </View>
+  );
+};
+
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
-
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View style={styles.screen}>
-      <Text>{selectedMeal.title}</Text>
-      <Button
-        title='Go Back to Categories'
-        onPress={() => {
-          props.navigation.popToTop();
-        }}
-      />
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText> {selectedMeal.duration}m </DefaultText>
+        <DefaultText> {selectedMeal.complexity.toUpperCase()} </DefaultText>
+        <DefaultText> {selectedMeal.affordability.toUpperCase()} </DefaultText>
+      </View>
+      <Text style={styles.title}> Ingredients </Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+      <Text style={styles.title}> Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
@@ -43,10 +65,26 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  listItem: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
